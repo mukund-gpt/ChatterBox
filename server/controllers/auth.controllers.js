c8486e907d9274f98393c059bfcf9571066d2971
mukund-gpt <abhishek.jupyter@gmail.com>
2024-08-06 11:41:42 +0530

    logout

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -49,6 +49,33 @@ export const signup = async (req, res, next) => {
   }
 };
 
-export const login = (req, res) => {};
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    let validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User Not Found"));
 
-export const logout = (req, res) => {};
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Password"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json({
+      id: validUser._id,
+      username: validUser.username,
+      email: email,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logout = (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json({
+      message: "Logout Success",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
