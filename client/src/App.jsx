be727884cc91a9763f94cb1894a73b2da33f63c6
mukund-gpt <abhishek.jupyter@gmail.com>
2024-08-08 22:04:25 +0530

    login frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 // import './App.css'
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
     <>
-      <h1>ChatterBox</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <h1 className="text-green-400">ChatterBox</h1>
+      <button className="btn btn-outline btn-info">Info</button>
+      <button className="btn btn-outline btn-success">Success</button>
+      <button className="btn btn-outline btn-warning">Warning</button>
+      <button className="btn btn-outline btn-error">Error</button>
     </>
   );
 }
