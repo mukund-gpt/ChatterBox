0665a03f807c473984f1479f90a7f767080d9785
mukund-gpt <abhishek.jupyter@gmail.com>
2025-02-07 18:17:47 +0530

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const apiUrl = "http://localhost:3000";
 
 // https://vitejs.dev/config/
 export default defineConfig({
--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
